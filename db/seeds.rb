850176fea079406010c4ec1367f73e0c67fb24c5
huntojkojqw01 <hungnd.k58@gmail.com>
2017-01-06 00:16:00 +0700

    99,9%

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,6 +53,7 @@ def convertTime(thoigian)
 		end
 		return t
 end
+
 Chuongtrinhdaotao.destroy_all
 Dangkihocphan.destroy_all
 Dangkilophoc.destroy_all
@@ -73,65 +74,56 @@ i=100
 	Hocki.create!(mahocki:"#{nam}3",dinhmuchocphi:i+60,bd:Date.new(nam+1,7,10),kt:Date.new(nam+1,7,10)+1.months)
 	i+=100	
 end
-1.upto(10) do |i|
-	khoa=Khoavien.create(tenkhoavien:"Khoa,Vien #{i}",sodienthoai:Faker::PhoneNumber.phone_number,diadiem:"toa nha D#{i}")
+@hk=Hocki.count
+@ihk=Hocki.first.id
+1.upto(20) do |i|
+	Khoavien.create(tenkhoavien:"Khoa viện #{i}",sodienthoai:Faker::PhoneNumber.phone_number,diadiem:" C#{rand(10)}-#{rand(400)+101}")
 end
-1.upto(30) do |i|
-	tmp=Khoavien.count
-	tmp=rand(tmp)+Khoavien.first.id	
-	Hocphan.create!(mahocphan:"HP#{i}",tenhocphan:Faker::Name.name,tinchi:rand(3)+2,tinchihocphi:rand(5)+2,trongso:(rand(2)+7)*0.1,khoavien_id:tmp)
+@kv=Khoavien.count
+@ikv=Khoavien.first.id
+1.upto(50) do |i|	
+	Hocphan.create(mahocphan:"HP#{i}",tenhocphan:"Học phần #{i}",tinchi:rand(3)+2,tinchihocphi:rand(5)+2,trongso:(rand(2)+7)*0.1,khoavien_id:rand(@kv)+@ikv)
 end
-1.upto(10) do |i|
-	tmp=Khoavien.count
-	tmp=rand(tmp)+Khoavien.first.id	
-	Giaovien.create!(magiaovien:"GV#{i}",tengiaovien:Faker::Name.name,ngaysinh:Faker::Date.between_except(60.year.ago, 24.year.from_now, Date.today),email:(Faker::Internet.email),khoavien_id:tmp)
+@hp=Hocphan.count
+@ihp=Hocphan.first.id
+1.upto(200) do |i|	
+	Giaovien.create(magiaovien:"GV#{i}",tengiaovien:"Giáo viên #{i}",ngaysinh:Faker::Date.between(60.years.ago,22.years.ago),email:"gv#{i}@gmail.com",khoavien_id:rand(@kv)+@ikv)
 end
-1.upto(10) do |i|
-	tmp=Khoavien.count
-	tmp=rand(tmp)+Khoavien.first.id
-	tmp2=Giaovien.count
-	tmp2=rand(tmp2)+Giaovien.first.id
-	Lopsinhvien.create!(tenlopsinhvien:"Lop sinh vien #{i}",khoahoc:55+rand(7),giaovien_id:tmp2,khoavien_id:tmp)
+@gv=Giaovien.count
+@igv=Giaovien.first.id
+1.upto(100) do |i|	
+	Lopsinhvien.create(tenlopsinhvien:"Lớp sinh viên #{i}",khoahoc:55+rand(7),giaovien_id:rand(@gv)+@igv,khoavien_id:rand(@kv)+@ikv)
 end
-1.upto(30) do |i|
-	tmp=Lopsinhvien.count
-	tmp=rand(tmp)+Lopsinhvien.first.id
-	user=User.create!(name:"sv#{i}",password:"123456",password_confirmation:"123456",loai:"sv")
-	Sinhvien.create!(masinhvien:"SV#{i}",tensinhvien:Faker::Name.name,ngaysinh:Faker::Date.between(23.years.ago,18.years.ago),email:Faker::Internet.email,trangthai:Faker::Boolean.boolean,lopsinhvien_id:tmp,user_id:user.id)
+@lsv=Lopsinhvien.count
+@ilsv=Lopsinhvien.first.id
+1.upto(500) do |i|	
+	user=User.create(name:"sv#{i}",password:"123456",password_confirmation:"123456",loai:"sv")
+	Sinhvien.create(masinhvien:"SV#{i}",tensinhvien:"Sinh viên #{i}",ngaysinh:Faker::Date.between(30.years.ago,18.years.ago),email:"sv#{i}@gmail.com",trangthai:Faker::Boolean.boolean,lopsinhvien_id:rand(@lsv)+@ilsv,user_id:user.id)
 end
+@sv=Sinhvien.count
+@isv=Sinhvien.first.id
 hocphans=Hocphan.all
 hocphans.each.with_index do |hp,j|
-	1.upto(10) do |i|
-		tmp=Giaovien.count
-		tmp=rand(tmp)+Giaovien.first.id
-		tmp2=Hocki.count
-		tmp2=rand(tmp2)+Hocki.first.id
-		Lophoc.create(malophoc:"LH#{j*10+i}",thoigian:convertTime(randomTime),diadiem:"D#{j*10+i}",maxdangki:40+rand(10),hocphan_id:hp.id,giaovien_id:tmp,hocki_id:tmp2)
+	1.upto(200) do |i|		
+		Lophoc.create(malophoc:"LH#{j*10+i}",thoigian:convertTime(randomTime),diadiem:"D#{j*10+i}-#{rand(400)+100}",maxdangki:40+rand(10),hocphan_id:hp.id,giaovien_id:rand(@gv)+@igv,hocki_id:rand(@hk)+@ihk)
 	end
 end
-1.upto(10) do |i|
-		tmp=Sinhvien.count
-		tmp=rand(tmp)+Sinhvien.first.id
-		tmp2=Hocki.count
-		tmp2=rand(tmp2)+Hocki.first.id
-		tmp3=Hocphan.count
-		tmp3=rand(tmp3)+Hocphan.first.id	
-		Dangkihocphan.create(hocphan_id:tmp3,sinhvien_id:tmp,hocki_id:tmp2)
+@lh=Lophoc.count
+@ilh=Lophoc.first.id
+1.upto(100) do |i|			
+		Dangkihocphan.create(hocphan_id:rand(@hp)+@ihp,sinhvien_id:rand(@sv)+@isv,hocki_id:rand(@hk)+@ihk)
 end
-1.upto(30) do |i|
-		sinhvien=Sinhvien.offset(rand(Sinhvien.count)).first			
-		lophoc=Lophoc.offset(rand(Lophoc.count)).first		
+1.upto(500) do |i|	
+		lh=Lophoc.find(rand(@lh)+@ilh)		
 		diemquatrinh=rand(10)
 		diemthi=rand(10)
-		trongso=lophoc.hocphan.trongso
+		trongso=lh.hocphan.trongso
 		x=tinhDiem(diemquatrinh,diemthi,trongso)		
-		Dangkilophoc.create(diemquatrinh:diemquatrinh,diemthi:diemthi,diemso:x[0],diemchu:x[1],hesohocphi:rand(3)+1,lophoc_id:lophoc.id,sinhvien_id:sinhvien.id)
+		Dangkilophoc.create(diemquatrinh:diemquatrinh,diemthi:diemthi,diemso:x[0],diemchu:x[1],hesohocphi:rand(3)+1,lophoc_id:lh.id,sinhvien_id:rand(@sv)+@isv)
 end
-1.upto(50) do |i|		
-		tmp=Hocphan.count
-		tmp=rand(tmp)+Hocphan.first.id
-		tmp2=Lopsinhvien.count
-		tmp2=rand(tmp2)+Lopsinhvien.first.id
+1.upto(200) do |i|		
+		tmp=rand(@hp)+@ihp
+		tmp2=rand(@lsv)+@ilsv		
 		if Chuongtrinhdaotao.where("hocphan_id=? and lopsinhvien_id=?",tmp,tmp2).count==0		
 			Chuongtrinhdaotao.create(hocki:rand(10)+1,hocphan_id:tmp,lopsinhvien_id:tmp2)			
 		else
